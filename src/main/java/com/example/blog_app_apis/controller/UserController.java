92b487d4d6851438968234f36a8aacd87765bd20
anjvaidya26 <anjvaidya1964@gmail.com>
2025-03-24 14:03:05 +0530

    user create controller method

--- src/main/java/com/example/blog_app_apis/controller/UserController.java
+++ src/main/java/com/example/blog_app_apis/controller/UserController.java
@@ -1,5 +1,12 @@
 package com.example.blog_app_apis.controller;
 
+import com.example.blog_app_apis.payloads.UserDto;
+import com.example.blog_app_apis.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
-	
-	
+    @Autowired
+    private UserService userService;
+
+    //POST--create user
+    @PostMapping("/")
+    public ResponseEntity<UserDto> creteUser(@RequestBody UserDto userDto){
+        System.out.println("create user controller");
+        UserDto createUserDto = this.userService.createUser(userDto);
+        return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+    }
+
+
 }
