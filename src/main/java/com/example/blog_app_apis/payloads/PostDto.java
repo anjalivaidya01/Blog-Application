0c941eac9a209b65b1b4560c92ad547694c47a20
anjvaidya26 <anjvaidya1964@gmail.com>
2025-08-06 16:01:54 +0530

    added getter and setter to postDto

--- src/main/java/com/example/blog_app_apis/payloads/PostDto.java
+++ src/main/java/com/example/blog_app_apis/payloads/PostDto.java
@@ -2,9 +2,15 @@ package com.example.blog_app_apis.payloads;
 
 import com.example.blog_app_apis.entity.Category;
 import com.example.blog_app_apis.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class PostDto {
 
     private String title;
