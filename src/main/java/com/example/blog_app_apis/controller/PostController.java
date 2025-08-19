866de44957ed4f4c7f5dfbfaea3f758673cd03b8
anjvaidya26 <anjvaidya1964@gmail.com>
2025-08-19 23:36:13 +0530

    to serve the uploaded image

--- src/main/java/com/example/blog_app_apis/controller/PostController.java
+++ src/main/java/com/example/blog_app_apis/controller/PostController.java
@@ -7,14 +7,19 @@ import com.example.blog_app_apis.payloads.PostDto;
 import com.example.blog_app_apis.payloads.PostResponse;
 import com.example.blog_app_apis.services.FileService;
 import com.example.blog_app_apis.services.PostService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -142,6 +147,19 @@ public class PostController {
        PostDto updatePost = this.postService.updatePost(postDto,postId);
        return new ResponseEntity<PostDto>(updatePost, HttpStatus.OK);
 
+
+    }
+
+    //method to serve files
+    @GetMapping(value = "/post/image/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(
+            @PathVariable("imageName") String imageName,
+            HttpServletResponse response
+    ) throws IOException {
+
+        InputStream resource = this.fileService.getResource(path, imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource,response.getOutputStream());
 
     }
 }
