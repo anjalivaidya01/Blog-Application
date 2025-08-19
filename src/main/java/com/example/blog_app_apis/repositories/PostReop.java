2bbe55b04a68d8de76bf719534a9bc9b40290bab
anjvaidya26 <anjvaidya1964@gmail.com>
2025-08-19 21:27:58 +0530

    changed the search method with @query annotation because searching was  not proper

--- src/main/java/com/example/blog_app_apis/repositories/PostReop.java
+++ src/main/java/com/example/blog_app_apis/repositories/PostReop.java
@@ -4,6 +4,8 @@ import com.example.blog_app_apis.entity.Category;
 import com.example.blog_app_apis.entity.Post;
 import com.example.blog_app_apis.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,5 +15,6 @@ public interface PostReop extends JpaRepository<Post, Integer> {
 
     List<Post> findByCategory(Category category);
 
-    List<Post> findByTitleContaining(String title);
+    @Query("select p from Post p where p.title like :key")
+    List<Post> searchByTitle(@Param("key") String title);
 }
