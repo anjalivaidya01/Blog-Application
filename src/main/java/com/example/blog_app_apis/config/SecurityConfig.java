ebc3d8ad2184e0c66fc5643c01846bac385cd1f9
anjvaidya26 <anjvaidya1964@gmail.com>
2025-08-20 11:05:44 +0530

    configuration for password encoder

--- src/main/java/com/example/blog_app_apis/config/SecurityConfig.java
+++ src/main/java/com/example/blog_app_apis/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.example.blog_app_apis.config;
 
+import com.example.blog_app_apis.security.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,6 +21,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private final  CustomUserDetailService customUserDetailService;
+
+
+    public SecurityConfig(CustomUserDetailService customUserDetailService) {
+        this.customUserDetailService = customUserDetailService;
+    }
 
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
@@ -37,6 +50,32 @@ public class SecurityConfig {
         return http.build();
     }
 
+//
+//    @Override
+//    protected  void configure(AuthticationManagerBuilder auth) throws Exception{
+//        auth.userDetailsService(this.customUserDetailService).passwordEncoder();
+//    }
+
+    // Authentication manager with custom userDetailsService
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authBuilder
+                .userDetailsService(customUserDetailService)
+                .passwordEncoder(passwordEncoder());
+
+        return authBuilder.build();
+    }
+
+
+    // Password encoder
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 
 }
