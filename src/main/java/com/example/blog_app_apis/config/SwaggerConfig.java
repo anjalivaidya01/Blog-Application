fecc99e739f77530d0b79f58057c0f012f8363ef
anjvaidya26 <anjvaidya1964@gmail.com>
2025-08-20 22:46:57 +0530

    done authorization functionality to swagger documentation

--- src/main/java/com/example/blog_app_apis/config/SwaggerConfig.java
+++ src/main/java/com/example/blog_app_apis/config/SwaggerConfig.java
@@ -1,22 +1,36 @@
 package com.example.blog_app_apis.config;
 
 
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import jdk.javadoc.doclet.Doclet;
 import jdk.jshell.SourceCodeAnalysis;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springdoc.core.configuration.SpringDocKotlinConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.lang.annotation.Documented;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    public static final String AUTHORIZATION_HEADER="Authorization";
+
+
+
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
@@ -29,14 +43,49 @@ public class SwaggerConfig {
                                 .name("Anjali")
                                 .url("https://google.com")
                                 .email("anj@gmail.com"))
-                        .license(new io.swagger.v3.oas.models.info.License()
+                        .license(new License()
                                 .name("License of APIs")
                                 .url("API License URL"))
+                )
+                // JWT Security Configuration
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .name(AUTHORIZATION_HEADER)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
                 );
     }
+
+//    private ApiKey apiKey(){
+//        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+//    }
+//
+//private List<SecurityContext> securityContexts(){
+//        return Arrays.asList(SecurityContext.builder().securityRefrences(null).build());
+//}
+//
+//private Lists<SecurityRefrences> sf()
+//{
+//
+//    AuthorizationScope scope = new AuthorizationScope("global","accessEverything");
+//  return Arrays.asList(new SecurityRefrences("JWT", new AuthorizationScope[] { scope}) );
+//}
+//
+//
+//
 //@Bean
 //    public Docket api(){
-//        return new Docket(DocumentationType.SWAGGER_2).apiInfo(getInfo()).select().apis(RequestHeaderSelectors.any()).path(PathSelectors.any()).build();
+//        return new Docket(DocumentationType.SWAGGER_2)
+//        .apiInfo(getInfo())
+//    .securityContexts(securityContexts())
+//    .securitySchemes(Arrays.asList(apikeys()))
+//        .select()
+//        .apis(RequestHeaderSelectors.any())
+//        .path(PathSelectors
+//        .any()).build();
 //    }
 //
 //    private ApiInfo getInfo(){
