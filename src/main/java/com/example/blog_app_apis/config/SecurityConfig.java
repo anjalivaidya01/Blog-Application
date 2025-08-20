20ce009c681ecb741519e1bf6550ed79b0c9005d
anjvaidya26 <anjvaidya1964@gmail.com>
2025-08-20 08:45:17 +0530

    tried to implement securityConfig

---
+++ src/main/java/com/example/blog_app_apis/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.blog_app_apis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        .csrf(csrf -> csrf.disable())   // instead of http.csrf().disable()
+        .authorizeHttpRequests(auth -> auth
+        .anyRequest().authenticated()
+                )
+        .httpBasic(Customizer.withDefaults()); // instead of http.httpBasic()
+
+        return http.build();
+    }
+
+
+
+}
