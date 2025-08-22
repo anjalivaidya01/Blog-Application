a7471775d0529f2f6e68af3ad84b61f88995e3ba
anjvaidya26 <anjvaidya1964@gmail.com>
2025-08-23 00:51:13 +0530

    final project added crossOrigin

--- src/main/java/com/example/blog_app_apis/controller/DefaultController.java
+++ src/main/java/com/example/blog_app_apis/controller/DefaultController.java
@@ -2,11 +2,13 @@ package com.example.blog_app_apis.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class DefaultController {
 
 
