5d692a676c329965009b3ba9806f9e9c7742153e
anjvaidya26 <anjvaidya1964@gmail.com>
2025-08-23 00:40:24 +0530

    final project some troubleshoot2

--- src/main/java/com/example/blog_app_apis/controller/DefaultController.java
+++ src/main/java/com/example/blog_app_apis/controller/DefaultController.java
@@ -4,14 +4,19 @@ package com.example.blog_app_apis.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class DefaultController {
 
 
-    @GetMapping("/")
-    public String redirectToSwagger() {
-        return "redirect:/swagger-ui/index.html";
-    }
+//    @GetMapping("/")
+//    public String redirectToSwagger() {
+//        return "redirect:/swagger-ui/index.html";
+//    }
+@GetMapping("/")
+public RedirectView redirectToSwagger() {
+    return new RedirectView("/swagger-ui/index.html");
+}
 
 }
